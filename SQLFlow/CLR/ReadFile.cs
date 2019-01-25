db7f3550562c4d662222ce6d74759653d719b5ec
Frederik Siegumfeldt <fsi@grit-solutions.ch>
2019-01-25 16:07:19 +0100

    Added new default schema [utility], added CLR wrapper for System.IO.File.ReadAllText

---
+++ SQLFlow/CLR/ReadFile.cs
@@ -0,0 +1,8 @@
+public partial class StoredProcedures
+{
+  [Microsoft.SqlServer.Server.SqlFunction]
+  public static string ReadFile(string path, string encoding)
+  {
+    return System.IO.File.ReadAllText(path, System.Text.Encoding.GetEncoding(encoding));
+  }
+}
