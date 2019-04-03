fce143458b949baa6c1c437584a31d320c5b4f8a
Frederik Siegumfeldt <fsi@grit-solutions.ch>
2019-04-03 10:07:49 +0200

    StatusCode made public

--- SQLFlowDotNet/Status.cs
+++ SQLFlowDotNet/Status.cs
@@ -3,12 +3,17 @@
     public class Status
     {
         private readonly FlowDatabase FlowDatabase;
-        private readonly string StatusCode;
+        public string StatusCode;
 
         internal Status(FlowDatabase flowDatabase, string statusCode)
         {
             FlowDatabase = flowDatabase;
             StatusCode = statusCode;
+        }
+
+        public override string ToString()
+        {
+            return StatusCode;
         }
     }
 }
