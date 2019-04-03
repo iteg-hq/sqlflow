64ebea73a36350c71a65af8d8ce80f5de50c812b
Frederik Siegumfeldt <fsi@grit-solutions.ch>
2019-04-03 10:07:38 +0200

    Database references removed

--- SQLFlow/Flows/Test/00 - Sample.sql
+++ SQLFlow/Flows/Test/00 - Sample.sql
@@ -1,23 +1,23 @@
 CREATE PROCEDURE flow_test.TestSetup
 AS
-EXEC [$(SQLFlow)].flow.AddType 'Test:TestFlow', @ExecutionGroupCode='System';
+EXEC flow.AddType 'Test:TestFlow', @ExecutionGroupCode='System';
 
-EXEC [$(SQLFlow)].flow.DropActions 'Test:TestFlow';
+EXEC flow.DropActions 'Test:TestFlow';
 
-EXEC [$(SQLFlow)].flow.AddAction 'Test:TestFlow.New.Start', 'Running';
-EXEC [$(SQLFlow)].flow.AddAction 'Test:TestFlow.Running.Complete', 'Complete';
-EXEC [$(SQLFlow)].flow.AddAction 'Test:TestFlow.Running.Fail', 'Failed';
-EXEC [$(SQLFlow)].flow.AddAction 'Test:TestFlow.Failed.Rollback', 'RollbackRunning';
-EXEC [$(SQLFlow)].flow.AddAction 'Test:TestFlow.RollbackRunning.Complete', 'RollbackCompleted';
-EXEC [$(SQLFlow)].flow.AddAction 'Test:TestFlow.RollbackCompleted.Restart', 'Running';
+EXEC flow.AddAction 'Test:TestFlow.New.Start', 'Running';
+EXEC flow.AddAction 'Test:TestFlow.Running.Complete', 'Complete';
+EXEC flow.AddAction 'Test:TestFlow.Running.Fail', 'Failed';
+EXEC flow.AddAction 'Test:TestFlow.Failed.Rollback', 'RollbackRunning';
+EXEC flow.AddAction 'Test:TestFlow.RollbackRunning.Complete', 'RollbackCompleted';
+EXEC flow.AddAction 'Test:TestFlow.RollbackCompleted.Restart', 'Running';
 
-EXEC [$(SQLFlow)].flow.SetStatusProcedure 'Test:TestFlow.Running', '$(DatabaseName).flow_test.FailOnce'
-EXEC [$(SQLFlow)].flow.SetStatusProcedure 'Test:TestFlow.RollbackRunning', '$(DatabaseName).flow_test.RollStuffBack'
+EXEC flow.SetStatusProcedure 'Test:TestFlow.Running', '$(DatabaseName).flow_test.FailOnce'
+EXEC flow.SetStatusProcedure 'Test:TestFlow.RollbackRunning', '$(DatabaseName).flow_test.RollStuffBack'
 
 GO
 
 CREATE PROCEDURE flow_test.Test
 AS
 DECLARE @FlowID INT;
-EXEC [$(SQLFlow)].flow.NewFlow 'Test:TestFlow', @FlowID OUTPUT;
+EXEC flow.NewFlow 'Test:TestFlow', @FlowID OUTPUT;
 ;
