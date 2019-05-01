19c732416618eb2ae4eb4ff099bf4e318b2846e8
Frederik Siegumfeldt <fsi@grit-solutions.ch>
2019-05-01 13:48:56 +0200

    Named DEFAULT constraints
    Better, but still doesn't prevent SqlPackage from drop/creating them :crying_cat_face:

--- SQLFlow/Tables/Flow.sql
+++ SQLFlow/Tables/Flow.sql
@@ -3,9 +3,9 @@ CREATE TABLE flow_internals.Flow (
   , ExecutionGroupCode NVARCHAR(100) NOT NULL CONSTRAINT DF_Flow_ExecutionGroupCode DEFAULT ('Ungrouped')
   , TypeCode NVARCHAR(100) NOT NULL
   , StatusCode NVARCHAR(100) NULL
-  , CreatedAt DATETIME2(7) NOT NULL DEFAULT(SYSDATETIME())
+  , CreatedAt DATETIME2(7) NOT NULL CONSTRAINT DF_Flow_CreatedAt DEFAULT(SYSDATETIME())
   , ExecutedAt DATETIME2(7) NULL
-  , CreatedByLogin NVARCHAR(128) NULL DEFAULT(SUSER_NAME())
+  , CreatedByLogin NVARCHAR(128) NULL CONSTRAINT DF_Flow_CreatedByLogin DEFAULT(SUSER_NAME())
   , CONSTRAINT PK_Flow PRIMARY KEY (FlowID)
   , CONSTRAINT FK_Flow_FlowType FOREIGN KEY (TypeCode) REFERENCES flow_internals.FlowType (TypeCode)
   , CONSTRAINT FK_Flow_FlowStatus FOREIGN KEY (TypeCode, StatusCode) REFERENCES flow_internals.FlowStatus (TypeCode, StatusCode)
