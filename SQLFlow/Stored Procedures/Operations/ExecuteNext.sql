a168f04a1ed0ad0eb335d735c786a1146d4f26dc
Frederik Siegumfeldt <fsi@grit-solutions.ch>
2019-06-13 15:21:59 +0200

    ExecuteNext factored out of Main to keep the previous behavior of Main

---
+++ SQLFlow/Stored Procedures/Operations/ExecuteNext.sql
@@ -0,0 +1,19 @@
+CREATE PROCEDURE flow.ExecuteNext
+    @ExecutionGroupCode NVARCHAR(100) = 'Ungrouped'
+  , @ActionCode NVARCHAR(50) = 'Start'
+  , @SortOrder INT = 1
+AS
+SET NOCOUNT, XACT_ABORT ON;
+-- No logging, since this SP could be called very often.
+DECLARE @FlowID INT;
+DECLARE @RC INT;
+-- Try getting the next FlowID.
+EXEC @RC = flow_internals.GetNext @FlowID OUTPUT, @ExecutionGroupCode, @ActionCode, @SortOrder
+-- If we got one, execute it.
+IF @RC = 0
+BEGIN
+  EXEC flow.StartExecution @FlowID;
+  EXEC flow.Do @FlowID, @ActionCode;
+  EXEC flow.StopExecution @FlowID;
+END
+RETURN @RC
--- SQLFlow/Stored Procedures/Operations/Main.sql
+++ SQLFlow/Stored Procedures/Operations/Main.sql
@@ -4,15 +4,9 @@ CREATE PROCEDURE flow.Main
   , @SortOrder INT = 1
 AS
 SET NOCOUNT, XACT_ABORT ON;
--- No logging, since this SP could be called very often.
-DECLARE @FlowID INT;
-DECLARE @Done INT;
-EXEC @Done = flow_internals.GetNext @FlowID OUTPUT, @ExecutionGroupCode, @ActionCode, @SortOrder
-WHILE @Done = 0
-BEGIN
-  EXEC flow.StartExecution @FlowID;
-  EXEC flow.Do @FlowID, @ActionCode;
-  EXEC flow.StopExecution @FlowID;
-
-  EXEC @Done = flow_internals.GetNext @FlowID OUTPUT, @ExecutionGroupCode, @ActionCode, @SortOrder
-END
+-- (No logging, since this SP could be called very often)
+-- Execute next, and keep going until
+DECLARE @RC INT;
+EXEC @RC = flow.ExecuteNext @ExecutionGroupCode, @ActionCode, @SortOrder
+WHILE @RC = 0
+  EXEC @RC = flow.ExecuteNext @ExecutionGroupCode, @ActionCode, @SortOrder
