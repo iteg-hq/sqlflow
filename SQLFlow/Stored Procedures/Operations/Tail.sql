06dafb9814dc8124f8c5828aec586b28d0f14149
Frederik Siegumfeldt <fsi@iteg.dk>
2019-08-15 14:25:40 +0200

    Fixed log entry count capping

--- SQLFlow/Stored Procedures/Operations/Tail.sql
+++ SQLFlow/Stored Procedures/Operations/Tail.sql
@@ -1,6 +1,18 @@
-CREATE PROCEDURE flow.Tail @rv BINARY(8) = NULL
+CREATE PROCEDURE flow.Tail
+    @LogLevelCode NVARCHAR(10) = 'INFO'
+  , @rv BINARY(8) = NULL
 AS
 SET NOCOUNT, XACT_ABORT ON;
+
+DECLARE @LogLevelID TINYINT;
+SELECT @LogLevelID = LogLevelID
+FROM flow_internals.LogLevel
+WHERE LogLevelCode = @LogLevelCode
+;
+
+-- Cap the number of entries returned to 100.
+-- Get the latest 101 entries - the rv of the earliest of these is the last entry we don't want to see.
+-- That leaves 100 entries, max. (Note: If there are less that 100 relevant entries, we never show the first one)
 IF @rv IS NULL
 SELECT @rv = MIN(rv)
 FROM (
@@ -8,6 +20,7 @@ FROM (
         rv
       , ROW_NUMBER() OVER (ORDER BY rv DESC) AS n
     FROM flow.LogEntry
+    WHERE LogLevelID >= @LogLevelID
   ) AS t
 WHERE n <= 101
 ;
@@ -15,4 +28,5 @@ WHERE n <= 101
 SELECT [rv], [LogLevelCode], [LogLevelID], [EntryTimestamp], [FormattedEntryText], [ServerProcessID], [FlowID], [StatusCode], [UserName], [ServerName]
 FROM flow.LogEntry
 WHERE rv > @rv
+  AND LogLevelID >= @LogLevelID
 ORDER BY rv;
