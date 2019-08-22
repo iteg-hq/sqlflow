5fac86ee2a2d331d98a5ed4820718fcece5d56e1
Frederik Siegumfeldt <fsi@iteg.dk>
2019-08-22 13:51:19 +0200

    Improved logging
     - Logs "Lock ... already held" and exits if the lock is already held by the flow.
     - Logs the ID of the holding flow (one of them), if the lock (or a child lock) is already held by another flow.

--- SQLFlow/Stored Procedures/Internal/AcquireLock.sql
+++ SQLFlow/Stored Procedures/Internal/AcquireLock.sql
@@ -6,15 +6,27 @@ SET NOCOUNT, XACT_ABORT ON;
 EXEC flow_internals.UpdateContext @FlowID;
 EXEC flow.Log 'TRACE', 'AcquireLock [:1:], [:2:]', @FlowID, @RootLockCode;
 
+-- Check if the lock is held by the flow.
+IF EXISTS (
+    SELECT 1
+    FROM flow_internals.Lock AS l
+    WHERE LockCode = @RootLockCode
+      AND HeldByFlowID = @FlowID
+  )
+BEGIN
+  EXEC flow.Log 'DEBUG', 'Lock [:1:] already held.', @RootLockCode, @@ROWCOUNT;
+  RETURN
+END
+-- If not, try acquiring it.
 BEGIN TRANSACTION
   CREATE TABLE #tree (LockCode NVARCHAR(200) PRIMARY KEY, LockLevel INT NOT NULL);
 
   EXEC flow_internals.ReleaseLock @FlowID;
-
+  -- Root lock and children - if any one of these is held, the lock cannot be acquired
   ;WITH tree AS (
       SELECT LockCode, 0 AS LockLevel
       FROM flow.Lock
-      WITH (TABLOCKX, HOLDLOCK)
+      WITH (TABLOCKX, HOLDLOCK) -- Locks the lock table
       WHERE LockCode = @RootLockCode
       UNION ALL
       SELECT child.LockCode, tree.LockLevel+1
@@ -26,9 +38,12 @@ BEGIN TRANSACTION
   SELECT LockCode, LockLevel
   FROM tree
 
-  DECLARE @UnavailableLockCode NVARCHAR(200)
+  DECLARE @UnavailableLockCode NVARCHAR(200);
+  DECLARE @HeldBy INT;
 
-  SELECT TOP 1 @UnavailableLockCode = l.LockCode
+  SELECT TOP 1
+      @UnavailableLockCode = l.LockCode
+    , @HeldBy = HeldByFlowID
   FROM flow.Lock AS l
   INNER JOIN #tree AS t
     ON t.LockCode = l.LockCode
@@ -38,7 +53,7 @@ BEGIN TRANSACTION
   IF @UnavailableLockCode != ''
   BEGIN
     ROLLBACK TRANSACTION
-    EXEC flow.Log 'ERROR', 'Could not acquire lock [:1:], :2: is already held', @RootLockCode, @UnavailableLockCode;
+    EXEC flow.Log 'ERROR', 'Could not acquire lock [:1:], :2: is already held by :3:', @RootLockCode, @UnavailableLockCode, @HeldBy;
     THROW 51000, 'Could not acquire lock', 1;
   END
 
