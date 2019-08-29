f3f2bd48a42787ba079f347e65291c472ebe5341
Frederik Siegumfeldt <fsi@iteg.dk>
2019-08-29 14:07:08 +0200

    FlowID på Execution

--- SQLFlow/Tables/Execution.sql
+++ SQLFlow/Tables/Execution.sql
@@ -1,7 +1,7 @@
 CREATE TABLE flow_internals.Execution (
     ExecutionID INT NOT NULL IDENTITY(1,1)
   , ParentExecutionID INT NULL
-  , FlowID INT NULL
+  , FlowID INT NULL CONSTRAINT DF_Execution_FlowID DEFAULT (CAST(SESSION_CONTEXT(N'FlowID') AS INT))
   , ExecutableName NVARCHAR(500) NOT NULL
   , ExecutionStartedAt DATETIME2(3) NOT NULL
   , ExecutionEndedAt DATETIME2(3) NULL
